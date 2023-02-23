1b628a828cf41918868ea2201b4b5fb04d36ffd8
besiki bakhtidze <bbakhtidze@gmail.com>
2023-02-23 19:10:21 +0400

    jss

---
+++ script.js
@@ -0,0 +1,24 @@
+let number = 100;
+if (number < 50) {
+  console.log("naklebia 50ze");
+} else if (number > 20) {
+  console.log("metia 20 ze");
+} else {
+  console.log("false");
+}
+
+let User = "mariam";
+if ((User = "mariam")) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let User = "mariam";
+switch (User) {
+  case "mariam":
+    console.log(true);
+
+    break;
+  case "false":
+}
